db93023d80b6148266fb231f76b45acf039edbfe
Qodesmith <theqodesmith@gmail.com>
2026-03-22 07:16:15 -0400

    New script to deterministically clean up worktrees

---
+++ evalCleanup.ts
@@ -0,0 +1,25 @@
+import { $ } from "bun";
+import path from "node:path";
+
+const currentBranch = (await $`git branch --show-current`.text()).trim();
+const rawWorktreeListOutput = await $`git worktree list --no-porcelain`.text();
+const worktreeRawData = rawWorktreeListOutput.split("\n").filter((line) => {
+  return line && !line.includes(`[${currentBranch}]`);
+});
+const worktreePathData = worktreeRawData.reduce<
+  { worktreeName: string; worktreePath: string }[]
+>((acc, data) => {
+  const [worktreePath] = data.split(" ");
+
+  if (worktreePath) {
+    // https://nodejs.org/docs/latest/api/path.html#pathparsepath
+    const worktreeName = path.parse(worktreePath).base;
+    acc.push({ worktreeName, worktreePath });
+  }
+
+  return acc;
+}, []);
+
+for (const { worktreeName, worktreePath } of worktreePathData) {
+  await $`git worktree remove ${worktreeName} --force`;
+}
